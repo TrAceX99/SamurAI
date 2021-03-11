d61899b6e7f403985c1c037735f37df5c3942962
TrAceX <djole1271952@gmail.com>
2021-03-11 22:37:05 +0100

    Find possible moves

--- Game.py
+++ Game.py
@@ -9,18 +9,137 @@ class Game(easyAI.TwoPlayersGame):
         self.stack = Stack()
 
     def _calcTile(self, x, y, dir):
-        pass
+        if dir == 's':
+            x += 2
+        elif dir == 'w':
+            x -= 2
+        elif dir == 'd':
+            if x % 2 == 1:
+                y += 1
+            x += 1
+        elif dir == 'e':
+            if x % 2 == 1:
+                y += 1
+            x -= 1
+        elif dir == 'q':
+            if x % 2 == 0:
+                y -= 1
+            x -= 1
+        elif dir == 'a':
+            if x % 2 == 0:
+                y -= 1
+            x += 1
+        else:
+            return None, None
+
+        if x > 8 or x < 0:
+            return None, None
+        if y > 26 or y < 0:
+            return None, None
+        return x, y
         
 
     def possible_moves(self):
         currentPlayerInfo = None
+        opponentInfo = None
         if self.player == self.players[0]:
             currentPlayerInfo = self.gameInfo['player1']
+            opponentInfo = self.gameInfo["player2"]
         else:
             currentPlayerInfo = self.gameInfo['player2']
+            opponentInfo = self.gameInfo["player1"]
+        
         mapInfo = self.gameInfo['map']['tiles']
+        playerx = currentPlayerInfo["x"]
+        playery = currentPlayerInfo["y"]
+        playerEnergy = currentPlayerInfo["energy"]
 
         possibleMoves = []
+
+        #move moves
+        x = playerx
+        y = playery
+        i = 1
+        while i <= playerEnergy:
+            x, y = self._calcTile(x, y, 's')
+            if x == None or mapInfo[x][y]["ownedByTeam"] != "" or mapInfo[x][y]["tileContent"]["itemType"] == "HOLE":
+                break
+            possibleMoves.append((0, 's', i))
+            i += 1
+        x = playerx
+        y = playery
+        i = 1
+        while i <= playerEnergy:
+            x, y = self._calcTile(x, y, 'w')
+            if x == None or mapInfo[x][y]["ownedByTeam"] != "" or mapInfo[x][y]["tileContent"]["itemType"] == "HOLE":
+                break
+            possibleMoves.append((0, 'w', i))
+            i += 1
+        x = playerx
+        y = playery
+        i = 1
+        while i <= playerEnergy:
+            x, y = self._calcTile(x, y, 'd')
+            if x == None or mapInfo[x][y]["ownedByTeam"] != "" or mapInfo[x][y]["tileContent"]["itemType"] == "HOLE":
+                break
+            possibleMoves.append((0, 'd', i))
+            i += 1
+        x = playerx
+        y = playery
+        i = 1
+        while i <= playerEnergy:
+            x, y = self._calcTile(x, y, 'e')
+            if x == None or mapInfo[x][y]["ownedByTeam"] != "" or mapInfo[x][y]["tileContent"]["itemType"] == "HOLE":
+                break
+            possibleMoves.append((0, 'e', i))
+            i += 1
+        x = playerx
+        y = playery
+        i = 1
+        while i <= playerEnergy:
+            x, y = self._calcTile(x, y, 'q')
+            if x == None or mapInfo[x][y]["ownedByTeam"] != "" or mapInfo[x][y]["tileContent"]["itemType"] == "HOLE":
+                break
+            possibleMoves.append((0, 'q', i))
+            i += 1
+        x = playerx
+        y = playery
+        i = 1
+        while i <= playerEnergy:
+            x, y = self._calcTile(x, y, 'a')
+            if x == None or mapInfo[x][y]["ownedByTeam"] != "" or mapInfo[x][y]["tileContent"]["itemType"] == "HOLE":
+                break
+            possibleMoves.append((0, 'a', i))
+            i += 1
+
+        #SkipATurn move
+        if currentPlayerInfo["numOfSkipATurnUsed"] < 5:
+            possibleMoves.append((1, None, None))
+
+        #StealKoalas move
+        opponentx = opponentInfo["x"]
+        opponenty = opponentInfo["y"]
+
+        x, y = self._calcTile(playerx, playery, 'a')
+        if x == opponentx and y == opponenty:
+            possibleMoves.append((2, None, None))
+        x, y = self._calcTile(playerx, playery, 's')
+        if x == opponentx and y == opponenty:
+            possibleMoves.append((2, None, None))
+        x, y = self._calcTile(playerx, playery, 'd')
+        if x == opponentx and y == opponenty:
+            possibleMoves.append((2, None, None))
+        x, y = self._calcTile(playerx, playery, 'e')
+        if x == opponentx and y == opponenty:
+            possibleMoves.append((2, None, None))
+        x, y = self._calcTile(playerx, playery, 'w')
+        if x == opponentx and y == opponenty:
+            possibleMoves.append((2, None, None))
+        x, y = self._calcTile(playerx, playery, 'q')
+        if x == opponentx and y == opponenty:
+            possibleMoves.append((2, None, None))
+
+        return possibleMoves
 
     def make_move(self, move):
 
