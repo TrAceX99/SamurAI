892c834769b4fe0f374559ab5f7fc869f60301a6
Blizzsuxx <61090552+Blizzsuxx@users.noreply.github.com>
2021-03-11 22:53:00 +0100

    undo move

--- Game.py
+++ Game.py
@@ -6,7 +6,14 @@ class Game(easyAI.TwoPlayersGame):
         self.players = players 
         self.nplayer = 1 # koji igrac igra, 1 ili 2
         self.gameInfo = None
-        self.stack = Stack()
+        self.FINITE_STATE_MACHINE = {
+            'q' : 'd',
+            'w' : 's',
+            'e' : 'a',
+            'a' : 'e',
+            's' : 'w',
+            'd' : 'q'
+        }
 
     def _calcTile(self, x, y, dir):
         pass
@@ -24,11 +31,26 @@ class Game(easyAI.TwoPlayersGame):
 
     def make_move(self, move):
 
-        self.stack.push(move)
+
+        currentPlayerInfo = None
+        if self.player == self.players[0]:
+            currentPlayerInfo = self.gameInfo['player1']
+        else:
+            currentPlayerInfo = self.gameInfo['player2']
+        mapInfo = self.gameInfo['map']['tiles']
+
+        move.info = []
         if move[0] == 0:
             for i in range(move[2]):
-                newPos = self._calcTile()
-            pass
+                x, y = self._calcTile(currentPlayerInfo['x'], currentPlayerInfo['y'], move[1])
+                currentPlayerInfo['x'] = x
+                currentPlayerInfo['y'] = y
+                value = self.getMoveValue(currentPlayerInfo, mapInfo[x][y]["tileContent"]['itemType'])
+                currentPlayerInfo['score'] += value
+                move.info(mapInfo[x][y]["tileContent"]['itemType'])
+                mapInfo[x][y]["tileContent"]['itemType'] = "EMPTY"
+                mapInfo[x][y]["tileContent"]["numOfItems"] = 0
+            mapInfo[x][y]['ownedByTeam'] = currentPlayerInfo['teamName']
         return
 
     def is_over(self):
@@ -36,11 +58,14 @@ class Game(easyAI.TwoPlayersGame):
 
     def scoring(self):
         currentPlayerInfo = None
+        otherPlayerInfo = None
         if self.player == self.players[0]:
             currentPlayerInfo = self.gameInfo['player1']
+            otherPlayerInfo = self.gameInfo['player2']
         else:
             currentPlayerInfo = self.gameInfo['player2']
-        return currentPlayerInfo['score']
+            otherPlayerInfo = self.gameInfo['player1']
+        return currentPlayerInfo['score'] - otherPlayerInfo['score']
 
     @staticmethod
     def quickSetup():
@@ -59,4 +84,27 @@ class Game(easyAI.TwoPlayersGame):
     def _calcTile(self, x, y, dir):
         pass
 
-    #def unmake_move(self, move): how to unmake a move (speeds up the AI)
+    def unmake_move(self, move):
+
+        direction = self.FINITE_STATE_MACHINE[move[1]]
+        mapInfo = self.gameInfo['map']['tiles']
+
+        currentPlayerInfo = None
+        if self.player == self.players[0]:
+            currentPlayerInfo = self.gameInfo['player1']
+        else:
+            currentPlayerInfo = self.gameInfo['player2']
+
+
+        itemType = move.info.pop()
+
+        currentPlayerInfo['score'] -= self.getMoveValue(currentPlayerInfo, itemType)
+        mapInfo[currentPlayerInfo['x']][currentPlayerInfo['y']]['ownedByTeam'] = ""
+        mapInfo[currentPlayerInfo['x']][currentPlayerInfo['y']]['itemType'] = move.info.pop()
+        #mapInfo[currentPlayerInfo['x']][currentPlayerInfo['y']]
+        
+        x, y = self._calcTile(currentPlayerInfo['x'], currentPlayerInfo['y'], direction)
+        currentPlayerInfo['x'] = x
+        currentPlayerInfo['y'] = y
+
+        pass
