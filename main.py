a7ddbce50df7310f54466f4036d37ddb552c8222
TrAceX <djole1271952@gmail.com>
2021-03-12 07:35:11 +0100

    aaaaaaaaaaaaaaa kill me

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
     else:
         gameid = int(sys.argv[1])
         game = Game.startGame(api.startGame(gameid), depth=6)
-        print(game.gameInfo)
+        print(game.gameInfo["numOfMove"])
 
     while game.gameInfo["finished"] == False:
         moves = game.possible_moves()
